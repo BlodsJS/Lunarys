3a69034c93b4edc5ba404af31fd8f8327c0e3e7d
BlodsJS <ible.cristhiancruz@gmail.com>
2026-04-08 11:03:54 -0300

    corrigindo função fantasma

--- placeholder.js
+++ placeholder.js
@@ -1,6 +1,11 @@
 export default {
-  async test() { return true },
-  async single() { return [] },
-  batch() { return [] },
-  movie() { return [] }
-}
+  async single() {
+    return [];
+  },
+  batch() {
+    return [];
+  },
+  movie() {
+    return [];
+  },
+};
